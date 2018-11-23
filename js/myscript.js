5be7e55e307a0a57aa39e04d17c842bf6c757707
Tony Xie <651376379@qq.com>
2018-11-23 17:58:37 -0500

    Made finals changes

--- js/myscript.js
+++ js/myscript.js
@@ -9,7 +9,7 @@ var lastId = 0;
 function refresh(){
 	$.ajax({
 		type: 'GET' ,
-		url: 'http://ituwcssa.com:8080/result',
+		url: 'http://localhost:8080/result',
 		dataType: 'json',
 		success: function(response){
 			update(response);
@@ -20,47 +20,26 @@ function refresh(){
 
 function update(response){
 	console.log(response);
-
-	if(response.type == 'NewGroup' && response.currentGroup != lastGroup){
-		lastGroup = response.currentGroup;
-		updateCandidatesFlag = true;
-	}
-
+	
 	$('.highlight').removeClass('highlight');
-
+	
 	if (response.mode === 'vote' && response.state === "IDLE") {
 		$('h1').html("滑大校园偶像-敬请期待");
 		hideAll();
 		resetGlobalVars();
 	} else if (response.mode === 'vote') {
 		$("#poll").hide();
-	
+		
 		if (updateCandidatesFlag) {
 			updateCandidatesFlag = false;
 			updateCandidatesInfo(response);
 		}
-
+		
 		if (response.state == "SINGLE"){
-			if(response.type == 'NewGroup'){
-				$('#result').hide();
-				$("#single").show();
-				updateSingle(response);
-				$('h1').html('投票通道尚未开启');
-			} else {
-				$('#result').hide();
-				$("#single").hide();
-				$('h1').html('歌手演唱中');
-			}
+			hideAll();
+			$('h1').html('歌手演唱中');
 		} else if (response.state == "VOTING"){
-			
-			if(response.type == 'NewGroup'){
-				$('#result').hide();
-				$("#single").show();
-			} else {
-				$('#result').show();
-				$("#single").hide();
-			}
-			
+			showResult(response.data.length);
 			var s = response.timerRemain/1000;
 			var t = '投票通道已开启';
 			if (s <= 20) {
@@ -69,45 +48,26 @@ function update(response){
 			}
 			$('h1').html(t + '<br>' + s);
 			updateVotes(response);
-
+			
 		} else if (response.state == "VOTED"){
-			if(response.type == 'NewGroup'){
-				$('#result').hide();
-				$("#single").show();
-				$('h1').html('投票通道已关闭');
-			} else {
-				$('#result').show();
-				$("#single").hide();
-				$('h1').html('导师投票时间');
-			}
+			showResult(response.data.length);
+			$('h1').html('导师及大众评审投票时间');
 			updateVotes(response);
 		} else if (response.state === 'RESULT'){
-			$('#single').hide();
 			$("#result").show();
 			$('h1').html('投票结果');
 			updateVotes(response);
 			updateResults(response);
 		}
-
-                        
+		
 	}  else if (response.mode === 'poll' && pollflag) {
 		pollflag = false;
 		$('h1').html("抽奖");
 		$('#voting').hide();
+		$('#result2').hide();
 		$('#result').hide();
-		$('#single').hide();
 		$("#poll").show();
-
 		pollInterval = setInterval(function(){updatepoll(response);},30);
-	}
-}
-
-function updateSingle(response){
-	if(response.currentCandidateId != lastId && response.currentCandidateId != null){
-		lastId = response.currentCandidateId;
-		$('#candName').html(response.currentCandidateName);
-		$('#candId').html(response.currentCandidateId + '号选手');
-		$('#candImg').attr("src", "./img/pp/cp" + response.currentCandidateId +".PNG");
 	}
 }
 
@@ -118,19 +78,19 @@ function updatepoll(response){
 	var y = Math.floor((Math.random() * 4)); 
 	switch (y) {
 		case 0:
-			s = "647";
-			break;
+		s = "647";
+		break;
 		case 1:
-			s = "226";
-			break;
+		s = "226";
+		break;
 		case 2:
-			s = "416";
-			break;
+		s = "416";
+		break;
 		case 3:
-			s = "519";
-			break;
+		s = "519";
+		break;
 	}
-
+	
 	// random numbers from 0-9
 	var x1=Math.floor((Math.random()*10));
 	var x2=Math.floor((Math.random()*10));
@@ -139,7 +99,7 @@ function updatepoll(response){
 	var x5=Math.floor((Math.random()*10));
 	var x6=Math.floor((Math.random()*10));
 	var x7=Math.floor((Math.random()*10));
-
+	
 	$('#poll').html(s+x1+x2+x3+x4+x5+x6+x7);
 	// repeat 100 times
 	pollCtr++;
@@ -147,22 +107,21 @@ function updatepoll(response){
 	// display real value from server
 	if (pollCtr >= 100) {
 		clearInterval(pollInterval);
-		$("#poll").html(response.winner[1]+response.winner[2]+response.winner[3]+"XXX"
-			+response.winner[7]+response.winner[8]+response.winner[9]+response.winner[10]);
+		$("#poll").html(response.winner[2]+response.winner[3]+response.winner[4]+"XXX"
+		+response.winner[8]+response.winner[9]+response.winner[10]+response.winner[11]);
 	}
 }
 
 function setProgressBar(id, val) {
-    $("#c_prog"+id).css('height', val + "%");
+	$("#c_prog"+id).css('height', val + "%");
 }	
 
 
 // hide all sections, for resetting purpose
 function hideAll() {
-	$('#voting').hide();
 	$('#poll').hide();
 	$('#result').hide();
-    $('#single').hide();
+	$('#result2').hide();
 }
 
 function resetGlobalVars() {
@@ -174,103 +133,53 @@ function resetGlobalVars() {
 function updateCandidatesInfo(response) {
 	
 	var candidates = null;
-	
-	if(response.type == 'NewGroup'){
-		candidates = response.data.filter(x=>x.group == response.currentGroup);
-	} else {
-		candidates = response.data;
-	}
-
+	candidates = response.data;
 	var candidateNum = candidates.length;
-
+	let offset = (candidateNum == 2) ? 4 : 0;
 	for (var i = 0; i < candidateNum; i++) {
-		$('#r_id'+ i).html(candidates[i].id+"号");
-	}
-	for (var i = 0; i < candidateNum; i++) {
-		$('#r_name'+ i).html(candidates[i].name);
+		$('#r_name'+ (i+offset)).html(candidates[i].name);
 	}
 }
 
 function updateVotes(response) {
-
+	
 	var candidates = null;
-	if(response.type == 'NewGroup'){
-		candidates = response.data.filter(x=>x.group == response.currentGroup);
-	} else {
-		candidates = response.data;
-	}	
+	candidates = response.data;
 	var candidateNum = candidates.length;
-
-	if(candidateNum > 6){
-		$('#row2').show();
-	} else {
-		$('#row2').hide();
-	}
-
+	let offset = (candidateNum == 2) ? 4 : 0;
 	var pad = 1;
 	// result
-		
-	for (var i = 0; i < candidateNum; i++) {
-		$('#r_votes'+ i).html((candidates[i].vote)+" 票");
-	}
-
 	var max = -1;
 	for (var i = 0; i < candidateNum; i++) {
 		max = Math.max(candidates[i].vote, max);
 	}
 	max += pad;
-	// update progress bar
-	for (var i = 0; i < candidateNum ; i++) {
-		setProgressBar(i, (candidates[i].vote + pad) * 90 / max + 10);
+	
+	
+	for (var i = 0; i < candidateNum; i++) {
+		$('#r_votes'+ (i+offset)).html((candidates[i].vote)+" 票");
 	}
-}
+	
+	for (var i = 0; i < candidateNum ; i++) {
+		setProgressBar((i+offset), (candidates[i].vote + pad) * 90 / max + 10);
+	}
+} 
 
 
 function updateResults(response) {
-
-	var candidates = null;
 	
-	if(response.type == 'NewGroup'){
-		candidates = response.data.filter(x=>x.group == response.currentGroup);
-	} else {
-		candidates = response.data;
-	}
-	
-	var candidateNum = candidates.length;
-	var index;
+	var candidates = candidates = response.data;
+	let offset = (candidates.length == 2) ? 4 : 0;
 
+	let order = candidates.map((x,i)=>{return{votes: x.vote, ind: i}});
+	order.sort((a,b) => b.votes-a.votes);
+	let cutoff = order[0].votes;
+	winners = order.filter(x => x.votes >= cutoff).map(x=> x.ind);
 
-	var max = -1;
-
-	if(response.type == 'NewGroup'){
-		for (var i = 0; i < candidateNum; i++) {
-			var votes = candidates[i].vote;
-			if (votes > max) {
-				max = votes;
-				index = i;
-			}
-				// in case of multiple winners
-			else if (votes === max) {
-				index = index + "," + i;
-			}
-		}
-	} else {
-		let order = candidates.map((x,i)=>{return{votes: x.vote, ind: i}});
-		order.sort((a,b) => b.votes-a.votes);
-		let cutoff = order[2].votes;
-		index = order.filter(x => x.votes >= cutoff).map(x=> x.ind).join(',');
+	for (var i in winners) {
+		setHighlight(offset + winners[i]);
 	}
-
-	if (typeof index === "number") {
-		setHighlight(index);
-	}
-	if (typeof index === "string") {
-		var winners = index.split(",");
-		for (var i in winners) {
-			setHighlight(winners[i]);
-		}
-	}
-	$('#result').show();
+	showResult(candidates.length);
 }
 
 function setHighlight(number){
@@ -280,7 +189,18 @@ function setHighlight(number){
 	$("#c_prog"+ number).addClass("highlight");
 }
 
+function showResult(candidateNum){
+	if(candidateNum == 2){
+		$('#result2').show();
+		$('#result').hide();
+	} else {
+		$('#result').show();
+		$('#result2').hide();
+	}
+}
+
 $(document).ready(function(){
+	hideAll();
 	setInterval(refresh,1000);
 });
 
